dc3a2d043fbc46f49a4355ad9951bd727aaa4638
Tommy Jakobsen <et3859@Tommys-MacBook-Pro.local>
2022-05-13 06:20:59 +0200

    added code

---
+++ pingport.py
@@ -0,0 +1,29 @@
+async def wait_host_port(host, port, duration=10, delay=2):
+    """Repeatedly try if a port on a host is open until duration seconds passed
+    
+    Parameters
+    ----------
+    host : str
+        host ip address or hostname
+    port : int
+        port number
+    duration : int, optional
+        Total duration in seconds to wait, by default 10
+    delay : int, optional
+        delay in seconds between each try, by default 2
+    
+    Returns
+    -------
+    awaitable bool
+    """
+    tmax = time.time() + duration
+    while time.time() < tmax:
+        try:
+            _reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=5)
+            writer.close()
+            await writer.wait_closed()
+            return True
+        except:
+            if delay:
+                await asyncio.sleep(delay)
+    return False
